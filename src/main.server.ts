12a095c42bc8510d163e76a2cbe895763f559a54
Teng Wei Hao <weihatowin1@gmail.com>
2025-09-13 18:03:03 +0800

    FE - Initial Project Setup

--- src/main.server.ts
+++ src/main.server.ts
@@ -2,6 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+const bootstrap = (context: any) => {
+    return bootstrapApplication(AppComponent, {
+        providers: [
+        ...config.providers,
+        ],
+    }, context);
+}
 
 export default bootstrap;
